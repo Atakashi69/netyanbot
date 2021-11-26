9caf72b05d806c72aedb188622c9fc60f2f51f5b
Takashi Aoki | 隆 青木 <aoki.takashi2018@yandex.ru>
2021-11-26 11:41:38 +0500

    Update netyanbot.js

--- scripts/netyanbot.js
+++ scripts/netyanbot.js
@@ -21,6 +21,10 @@ ComfyJS.onRaid = (user, viewers, extra) => {
 
 var bearduckInterval = setInterval(() => {
     ComfyJS.Say("BearDuck");
+}, 60000 * 10);
+
+var wingsInterval = setInterval(() => {
+    ComfyJS.Say("/me ✿ Хочешь милые КФС крылья для геншина? Получи их за ДВЕ подарочные Тир 1 подписки или ОДНУ Тир 2/Тир 3 подписку! Более подробная информация здесь: https://www.hoyolab.com/article/1413459 ✿");
 }, 60000 * 15);
 
 async function biteUser(user1, user2) {
@@ -52,7 +56,7 @@ function hornyMeter(user) {
 }
 
 function wings(user) {
-    ComfyJS.Say(`@${user} -> /me ✿ Хочешь милые КФС крылья для геншина? Получи их за ДВЕ подарочные Тир 1 подписки или ОДНУ Тир 2/Тир 3 подписку! Более подробная информация здесь: https://www.hoyolab.com/article/1413459 ✿`);
+    ComfyJS.Say(`@${user} -> Для того, чтобы получить крылья, вы должны купить 2 сабки ВО ВРЕМЯ стрима в категории GENSHIN IMPACT. Более подробную информацию можно узнать здесь: https://www.hoyolab.com/article/1413459`);
 }
 
 async function currentSong(user) {
