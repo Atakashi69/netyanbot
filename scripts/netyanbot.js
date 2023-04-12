816f4a0fa5bca20fc47cf2f9e1e3d2e2e623ee10
Takashi Aoki | 隆 青木 <aoki.takashi2018@yandex.ru>
2023-04-12 17:28:12 +0500

    Update netyanbot.js

--- scripts/netyanbot.js
+++ scripts/netyanbot.js
@@ -9,19 +9,19 @@ var wobbleLineCount = 0,
 ComfyJS.Init(TWITCHUSER, OAUTH);
 
 ComfyJS.onCommand = (user, command, message, flags, extra) => {
-    if (command == "кусь" || command == "bite") {
-        if (!message) biteRandomUser(user);
-        else biteUser(user, message);
-    }
-    if (command == "онлифанс" || command == "onlyfans") ComfyJS.Say("https://clck.ru/3vyXS peepoShy");
+    if (command == "кусь" || command == "bite")
+        if (message) biteUser(user, message);
+    else if (command == "онлифанс" || command == "onlyfans") ComfyJS.Say("https://clck.ru/3vyXS peepoShy");
     else if (command == "хорни" || command == "horny") hornyMeter(user);
     else if (command == "песня" || command == "song" || command == "track" || command == "трек") currentSong(user);
     else if (command == "котик" || command == "kitty") kittyMeter(user);
     else if (command == "розыгрыш" || command == "giveaway") giveaway(user);
     else if (command == "высадка") forRaid();
     else if (command == "меч") swordMeter(user);
-    else if (command == "love") loveMeter(user, message);
-    else if (command == "hate" || command == "хейт") hateMeter(user, message);
+    else if (command == "love")
+        if (message) loveMeter(user, message);
+    else if (command == "hate" || command == "хейт")
+        if (message) hateMeter(user, message);
     else if (command == "old" || command == "олд") oldMeter(user);
 };
 
